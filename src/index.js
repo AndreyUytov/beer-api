dad0e8e4c4642f394d9cdacccecd7b73a6aa2901
Andrew <uytov1991@gmail.com>
2019-12-02 15:55:00 +0300

    Верстка главной страницы

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import './index.html'
+
+import './styles/style.scss'
+
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import {Provider} from 'react-redux'
+
+// import {store} from 'store'
+// import { App } from 'components/app'
+
+
+// ReactDOM.render(
+//     <Provider store={store}>
+//         <App/>
+//     </Provider>,
+//     document.getElementById('root')
+// )
